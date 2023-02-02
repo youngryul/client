335720759318863be67260e03b1f811fe7eca57a
PNS212624\Hansol <yrlee00@hansol.com>
2023-02-02 17:28:10 +0900

    2/2 commit

--- src/main/java/com/example/client/service/RestTemplateService.java
+++ src/main/java/com/example/client/service/RestTemplateService.java
@@ -1,5 +1,7 @@
 package com.example.client.service;
 
+import com.example.client.dto.UserResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -11,19 +13,32 @@ public class RestTemplateService {
 
     //http://localhost/api/server/hello
     // response
-    public String hello(){
+    public UserResponse hello(){
         URI uri = UriComponentsBuilder
                 .fromUriString("http://localhost:9090")
                 .path("/api/server/hello")
+                .queryParam("name","steve")
+                .queryParam("age",10)
                 .encode()
                 .build()
                 .toUri();
         System.out.println(uri.toString());
 
+        /* getforobject 방식
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(uri, String.class);
 
         return result;
+
+         */
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<UserResponse> result = restTemplate.getForEntity(uri, UserResponse.class);
+
+        System.out.println(result.getStatusCode());
+        System.out.println(result.getBody());
+
+        return result.getBody();
 
     }
 }
